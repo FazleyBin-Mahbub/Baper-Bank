24a3133a41f07cebe5f185228a08b237cd5f0840
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-08-15 19:44:49 +0600

    this is baper bank official

---
+++ JS/bank.js
@@ -0,0 +1,15 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+  // gerEmail
+  const emailField = document.getElementById("user-email");
+  const userMail = emailField.value;
+
+  const passField = document.getElementById("user-pass");
+
+  const userPass = passField.value;
+
+  if (userMail == "hasinapm@fazley.com" && userPass == "secret") {
+    window.location.href = "banking.html";
+  } else {
+    console.log("Invalid");
+  }
+});
